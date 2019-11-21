c73c551c2595ee1a47ae1856027191083a4a5fea
Emery Tate <tateejt@gmail.com>
2019-11-21 21:00:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,8 @@ class Show < ActiveRecord::Base
   has_many :characters
   belongs_to :network
   has_many :actors, through: :characters
+  
+  def actors_list
+    self.actors.map{|actor| actor.full_name}
+  end
 end
