f11394b08a8cf010082e96c648d495596fd0d2a0
Emery Tate <tateejt@gmail.com>
2019-11-21 21:12:55 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_characters.rb
+++ db/migrate/004_create_characters.rb
@@ -2,7 +2,6 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |t|
       t.string :name 
-      t.integer :actor_id 
       t.integer :show_id
     end
   end
