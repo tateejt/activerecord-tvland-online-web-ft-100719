4e54ba3d2833ae208aeebf84a93ff3edb8c6a180
Emery Tate <tateejt@gmail.com>
2019-11-21 21:14:45 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 9) do
-
-  create_table "actors", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-  end
+ActiveRecord::Schema.define(version: 7) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -32,6 +27,7 @@ ActiveRecord::Schema.define(version: 9) do
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.integer "network_id"
+    t.string "genre"
   end
 
 end
