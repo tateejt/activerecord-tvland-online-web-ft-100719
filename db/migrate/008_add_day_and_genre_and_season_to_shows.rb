e0faef15760158b7deac9a467b47d3e7c5831c9c
Emery Tate <tateejt@gmail.com>
2019-11-21 21:11:50 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_day_and_genre_and_season_to_shows.rb
+++ db/migrate/008_add_day_and_genre_and_season_to_shows.rb
@@ -1,4 +1,5 @@
-class AddDayAndGenreAndSeasonToShows
+class AddDayAndGenreAndSeasonToShows < ActiveRecord::Migration[5.1]
+  
   def change 
     add_column :shows, :day, :string 
     add_column :shows, :season, :string
